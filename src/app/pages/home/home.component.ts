c1388ba8ba0ca2a989abf388e48986668cf83a5f
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-12-18 22:58:10 -0300

    feat: config and styling content component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
+import { ContentComponent } from "../../shared/content/content.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ContentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/shared/content/content.component.ts
+++ src/app/shared/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ImagesComponent } from "../images/images.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [ImagesComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
